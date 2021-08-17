6a70c5926a062e36a79595c9f25c45991cb35032
tanyajun <2364896551@qq.com>
2021-08-17 23:39:35 +0800

    test

--- src/views/profile/NoPaid.vue
+++ src/views/profile/NoPaid.vue
@@ -5,19 +5,31 @@
 </template>
 <script>
 import contentOrder from "views/profile/contentOrder.vue";
+import { findOrderByState } from "network/order.js";
 export default {
   name: "NoPaid",
-  components:{
-      contentOrder
+  components: {
+    contentOrder,
   },
   data() {
     return {
       order: [],
     };
   },
-created() {
-     this.order = this.$store.state.orderlist.filter((item) => {
-      return item.state===0;
+  async created() {
+
+    if (this.$store.state.orderlist === null) {
+      console.log("have");
+      let orderlist = await findOrderByState(this.$store.state.personal._id);
+      // 保存到vuex中
+      this.$store.commit({
+        type: "saveOrder",
+        orderlist,
+      });
+    }
+    
+    this.order = this.$store.state.orderlist.filter((item) => {
+      return item.state === 0;
     });
   },
 };
--- src/views/profile/WaiteSend.vue
+++ src/views/profile/WaiteSend.vue
@@ -3,6 +3,7 @@
 </template>
 <script>
 import contentOrder from "views/profile/contentOrder.vue";
+import { findOrderByState } from "network/order.js";
 export default {
   components: { contentOrder },
   name: "WaiteSend",
@@ -11,10 +12,23 @@ export default {
       order: [],
     };
   },
-  created() {
+  async created() {
+    if (this.$store.state.orderlist === null) {
+      console.log("have");
+      let orderlist = await findOrderByState(this.$store.state.personal._id);
+      // 保存到vuex中
+      this.$store.commit({
+        type: "saveOrder",
+        orderlist,
+      });
+    }
+
     this.order = this.$store.state.orderlist.filter((item) => {
       return item.state === 2;
     });
+
+
+
   },
 };
 </script>
